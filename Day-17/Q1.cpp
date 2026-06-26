03f825e88dfe8f56f2caf9035b1881a460ee4165
shraddha123321 <awasthishraddha170@gmail.com>
2026-06-27 04:03:51 +0530

    Day-17 uplodaded

---
+++ Day-17/Q1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr1[100], arr2[100], arr3[200];
+    int n1, n2;
+
+    cout << "Enter size of first array: ";
+    cin >> n1;
+
+    cout << "Enter first array elements: ";
+    for(int i = 0; i < n1; i++)
+    {
+        cin >> arr1[i];
+    }
+
+    cout << "Enter size of second array: ";
+    cin >> n2;
+
+    cout << "Enter second array elements: ";
+    for(int i = 0; i < n2; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    for(int i = 0; i < n1; i++)
+    {
+        arr3[i] = arr1[i];
+    }
+
+    for(int i = 0; i < n2; i++)
+    {
+        arr3[n1 + i] = arr2[i];
+    }
+
+    cout << "Merged array is: ";
+
+    for(int i = 0; i < n1 + n2; i++)
+    {
+        cout << arr3[i] << " ";
+    }
+
+    return 0;
+}
